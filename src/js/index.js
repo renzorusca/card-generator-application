929f6dd9b1fe2f8f84768a34f6c3411c62ea774e
Renzo Rusca <renzo.rusca@gmail.com>
2018-09-25 00:49:21 +0000

    sending first

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,30 @@
-import $ from "jquery";
-import 'bootstrap'; //breathecode dom for more explicit errors
-import 'breathecode-dom'; //DOM override to make JS easier to use
+
 import '../style/index.scss';
 
 window.onload = function(){
-    console.log('Hello Rigo from the console!');
+  let cardsuit = generateRandomSuit();
+  if (cardsuit == "&diams;" || cardsuit == "&hearts;") {
+    document.querySelector(".top-suite").classList.add("suit-color-red");
+    document.querySelector(".bottom-suite").classList.add("suit-color-red");
+  } else {
+   document.querySelector(".top-suite").classList.add("suit-color-black");
+   document.querySelector(".bottom-suite").classList.add("suit-color-black");
+  }
+
+
+document.querySelector(".top-suite").innerHTML = cardsuit;
+document.querySelector(".bottom-suite").innerHTML = cardsuit;
+document.querySelector(".number").innerHTML = (generateRandomNumber());
+
 };
+
+  function generateRandomNumber(){
+  let numbers = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+  let indexNumbers = Math.floor(Math.random()*numbers.length);
+  return numbers[indexNumbers];
+}
+  function generateRandomSuit(){
+  var suit = ["&diams;","&spades;","&hearts;","&clubs;"];
+  let indexSuit = Math.floor(Math.random()*suit.length);
+  return suit[indexSuit];
+}
